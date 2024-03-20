edb3a2f1307749f43eabca4295b5f37777a1d5fb
wenbo yang <wenbo.yang.84@gmail.com>
2024-03-20 11:54:51 -0700

    first check in

---
+++ src/startServices/startServices.ts
@@ -0,0 +1,20 @@
+import * as child from 'child_process';
+
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+const CHISELIMAGESKELETONIZATIONPATH = '../Chisel-Image-Skeletonization';
+
+const bash = child.spawn('bash', ['./src/startServices/start_image_skeletonization_service.sh']);
+
+bash.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+bash.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+bash.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+}); 
+
+await new Promise(resolve => setTimeout(resolve, 5000));
---
+++ src/startServices/start_image_skeletonization_service.sh
@@ -0,0 +1,7 @@
+
+cd 
+ls
+nvm use
+npm install 
+npm run build
+npm run start
