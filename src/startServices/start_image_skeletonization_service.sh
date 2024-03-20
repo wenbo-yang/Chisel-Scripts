3d6f899a92e09087187f4cb9450537ac5190c28a
wenbo yang <wenbo.yang.84@gmail.com>
2024-03-20 12:54:44 -0700

    try

--- src/startServices/start_image_skeletonization_service.sh
+++ src/startServices/start_image_skeletonization_service.sh
@@ -1,6 +1,11 @@
 
-cd 
+cd ../Chisel-Image-Skeletonization
 ls
+
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+
 nvm use
 npm install 
 npm run build
